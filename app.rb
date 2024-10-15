522d041e26d93531d4a7eb05f12af7fa08c0ddb3
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-15 01:08:19 +0000

    finished site

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
 require 'rspotify'
+require 'json'
 
-RSpotify.authenticate(ENV.fetch('SPOTIFY_CLIENT_ID'), ENV.fetch('SPOTIFY_SERCRET_ID'))
+# Configure RSpotify with your Spotify credentials
+RSpotify.authenticate(ENV.fetch("SPOTIFY_CLIENT_ID"), ENV.fetch("SPOTIFY_SERCRET_ID"))
 
-get("/") do
-  @artist = RSpotify::Artist.search('Artist Name').first
-  erb(:homepage)
+# Home route
+get '/' do
+  erb(:index)
+end
+
+# Search route
+get('/search') do
+  @artist_name = params[:artist_name]
+  @artist = RSpotify::Artist.search(@artist_name).first
+
+  if @artist
+    @top_tracks = @artist.top_tracks(:US)
+    @albums = @artist.albums
+  else
+    @error = "Artist not found"
+  end
+
+  erb(:search)
 end
